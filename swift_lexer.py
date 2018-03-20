8d80e899311bca8ed0f988a2df255f0f40b6eb12
danny311296 <danny311296@github.com>
2018-03-20 21:53:48 +0530

    Basic Grammar added

--- swift_lexer.py
+++ swift_lexer.py
@@ -1,18 +1,22 @@
 import ply.lex as lex
 
-tokens = ['NUMBER','VAR','ID','WHITESPACE','ENTER','EQ']
+tokens = ['NUMBER','VAR','ID','WHITESPACE','ENTER','EQ','TYPE','COL']
 
 t_WHITESPACE = r'\ +'
 t_ENTER = r'\n'
 
-literals = [ '+' , '-' , '*' , '/' , '(' , ')' , '=']
+literals = [ '+' , '-' , '*' , '/' , '(' , ')' , '=' , ':']
 
-reserved = { 'var': 'VAR' }
+reserved = { 'var': 'VAR' ,'Int': 'TYPE', 'Float': 'TYPE' ,'Double':'TYPE'}
 
 tokens = tokens + list(reserved.values())
 
 def t_EQ(t):
 	r'='
+	return t
+
+def t_COL(t):
+	r':'
 	return t
 
 def t_NUMBER(t):
@@ -23,6 +27,10 @@ def t_NUMBER(t):
 def t_ID(t):
 	r'[a-z|A-Z][a-z|A-Z|0-9]*'
 	t.type = reserved.get(t.value,'ID')
+	return t
+
+def t_TYPE(t):
+	r'Int|Float|Double'
 	return t
 
 lexer = lex.lex(debug=1)
--- swift_parser.py
+++ swift_parser.py
@@ -2,11 +2,37 @@ import ply.yacc as yacc
 
 from swift_lexer import tokens
 
+def p_start(p):
+	'''start : statements 
+			| empty'''
+
+def p_empty(p):
+	'empty :'
+	pass
+
+def p_statements(p):
+	'statements : statement ENTER next_statement'
+
+def p_next_statement(p):
+	'''next_statement : statement ENTER next_statement 
+					| empty''' 
+
+def p_statement(p):
+	'''statement : assignment_statement 
+				| declaration_statement'''
+
 def p_assignment_statement(p):
-	'assignment_statement : VAR WHITESPACE ID WHITESPACE EQ WHITESPACE NUMBER'
+	'assignment_statement : ID WHITESPACE EQ WHITESPACE NUMBER'
+
+def p_declaration_statement(p):
+	'''declaration_statement : VAR WHITESPACE ID COL WHITESPACE TYPE 
+							| VAR WHITESPACE ID COL WHITESPACE TYPE WHITESPACE EQ WHITESPACE NUMBER
+							| VAR WHITESPACE ID WHITESPACE EQ WHITESPACE NUMBER '''
+
+
 
 parser = yacc.yacc()
 
 while(1):
-	s = input()
+	s = input() + '\n'
 	parser.parse(s)
