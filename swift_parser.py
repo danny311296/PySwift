937d5f28a679c34108934e16ff4e08e812da029e
danny311296 <danny311296@github.com>
2018-03-20 22:28:55 +0530

    Basic Symbol Table Made

--- swift_parser.py
+++ swift_parser.py
@@ -1,6 +1,9 @@
 import ply.yacc as yacc
 
 from swift_lexer import tokens
+from collections import defaultdict
+
+symbol_table = defaultdict(lambda: None)
 
 def p_start(p):
 	'''start : statements 
@@ -23,12 +26,23 @@ def p_statement(p):
 
 def p_assignment_statement(p):
 	'assignment_statement : ID WHITESPACE EQ WHITESPACE NUMBER'
+	if(symbol_table[p[1]]==None):
+		print('Variable Not initialized')
+	else:
+		symbol_table[p[1]] = p[5]
+	
 
 def p_declaration_statement(p):
 	'''declaration_statement : VAR WHITESPACE ID COL WHITESPACE TYPE 
 							| VAR WHITESPACE ID COL WHITESPACE TYPE WHITESPACE EQ WHITESPACE NUMBER
 							| VAR WHITESPACE ID WHITESPACE EQ WHITESPACE NUMBER '''
-
+	if(len(p)==7):
+		symbol_table[p[3]] = 'unintialized'
+	elif(len(p)==11):
+		symbol_table[p[3]] = p[10]
+	else:
+		symbol_table[p[3]] = p[7]
+	print(symbol_table)
 
 
 parser = yacc.yacc()
