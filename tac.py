23397643ea16d91a301c2c49874aafdb50365290
danny311296 <danny311296@github.com>
2018-04-18 14:16:31 +0530

    Tac Generation modification

--- tac.py
+++ tac.py
@@ -9,22 +9,35 @@ dictionary = defaultdict(lambda: None)
 line = astFileText[i]
 
 def generateCode(line):
-	global tempCount,operation
+	global tempCount,operation,i
 	if(line.split()[0]=="---assign"):
 		operation = 'assign'
-		lhs = line.split()[1]
+		lhsvar = line.split()[1]
 		if(line.split()[2] != 'Node'):
 			rhs = line.split()[2]
+			print(lhsvar + ' = ' + rhs)
+			i += 1
 		else:
-			rhsNode = line.split()[3]
 			expressions = str(" ".join(astFileText[i+1].split())).split('---expression')[1:]
 			expressions = expressions[::-1]
+			iterationCount = 0
 			for exp in expressions:
+				iterationCount += 1
 				e = exp.split()
 				lhs = e[0]
 				op = e[2]
 				op1 = e[4]
 				op2 = e[5]
+				if(iterationCount == len(expressions)):
+					if(op1.startswith('Node')):
+						op1 = dictionary[op1]
+					if(op2.startswith('Node')):
+						op2 = dictionary[op2]				
+					print('t' + str(tempCount) + ' = ' + str(op1) + " " + op + " " + str(op2))
+					dictionary[lhs] = 't' +str(tempCount)
+					print(lhsvar + ' = ' + 't' + str(tempCount) )
+					tempCount += 1
+					break
 				if(not(op1.startswith('Node')) and not(op2.startswith('Node'))):
 					print('t' + str(tempCount) + ' = ' + op1 + " " + op + " " + op2)
 					dictionary[lhs] = 't' + str(tempCount)
@@ -39,13 +52,14 @@ def generateCode(line):
 					tempCount += 1
 
 
+			i += 2
+
+
 while(1):
 	if(line.startswith("Node")):
 		i += 1
 		line = astFileText[i]
 		generateCode(line)
-		if(operation=='assign'):
-			i += 2
 		if(i>=length):
 			break
 		line = astFileText[i]
---
+++ test5.swift
@@ -0,0 +1,9 @@
+var a = 2*3*5+6*7
+var b = 45
+var c = 3/4
+var d: Int = 4+3
+d = 100
+for i in 1...2 {
+var l = 2
+var x = l+1
+}
